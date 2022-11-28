ca12caeb8fb6065fc67d671fa5cea9d9afc70a6f
Achala <ach.chathuranga@gmail.com>
2022-11-28 13:39:06 +0800

    adding new files

---
+++ Python_Functions/Python_arbitrary_arguments.py
@@ -0,0 +1,9 @@
+def greet(*names):
+    """ Python Arbitrary Arguments - This function greets all
+    the person in the names tuple."""
+
+    for name in names:
+        print("Hello", name)
+
+
+greet("Achala", "Chathuranga", "ach", "cha")
